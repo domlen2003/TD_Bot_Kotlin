8a4d53e85b19bfa318eb4912470bd647268f5d21
Dominik Lenz <dominik.lenz@hamibot.io>
2021-02-15 22:50:59 +0100

    added voice create

--- src/main/kotlin/constants/VOICE.kt
+++ src/main/kotlin/constants/VOICE.kt
@@ -1,6 +1,6 @@
 package constants
 
 object VOICE {
-    const val CREATE_CHANNEL_ID = ""
-    const val COMP_CREATE_CHANNEL_ID = ""
+    const val CREATE_CHANNEL_ID = "806517538686894110"
+    const val COMP_CREATE_CHANNEL_ID = "806517539315908620"
 }
